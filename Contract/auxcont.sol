169d3098567787cdfc0c3f15f0a35858a4992d01
shebright <officialshebright@gmail.com>
2025-10-09 10:51:29 +0530

    Update auxcont.sol

--- Contract/auxcont.sol
+++ Contract/auxcont.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message;
+
+    // Constructor sets an initial message
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    // Function to update the message
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+
+    // Function to read the message
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
 
